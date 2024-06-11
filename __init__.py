1b711468413b04065f9dd8f351470b0f8c4371ba
Negarasu <143909516+Negarasu@users.noreply.github.com>
2024-06-11 15:08:49 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
 
-@author: negar_hofism7
-"""
 #example:
-
-from greet.py import SayUCMPhoenix
-from functions.py import Green
-from UCMPhoenix.py import plt
+from greet import SayASLUMpy
+from functions import Green
+from ASLUMpy import Green
 
