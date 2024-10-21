c66fe9c33e0b4a090e99deac5ddc1606c52de883
Negarasu <143909516+Negarasu@users.noreply.github.com>
2024-10-20 21:48:42 -0700

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,25 @@
 # src/ASLUMpy/Calculator.py
+# calculator.py
+from Calculator import add, subract, multiply, divide  # Import Keff from functions.py
 
-from .functions import Keff  # Import Keff from functions.py
 
-def main():
-    result = Keff(0.5)
-    print(f"Keff result: {result}")
 
-if __name__ == "__main__":
-    main()
+def add(x, y):
+    """Add Function"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract Function"""
+    return x - y
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x * y
+
+def divide(x, y):
+    """Divide Function"""
+    if y == 0:
+        raise ValueError("Can not divide by zero!")
+    return x / y
+
 
